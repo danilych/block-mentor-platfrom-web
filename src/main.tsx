4d2c48fc02242bd0ba442c67020259be82574b25
tamadekk <186jnd@gmail.com>
2025-03-29 14:04:45 +0100

    refactor: update imports to use @ alias instead of relative paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import ParticleEffect from "./components/Canvas.tsx";
+import "@/index.css";
+import App from "@/App.tsx";
+import ParticleEffect from "@/components/Canvas.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
